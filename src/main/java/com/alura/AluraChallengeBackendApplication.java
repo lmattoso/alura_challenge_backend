3482988f26761d5d74a6e805d897372097e9f6eb
Luis Mattoso <lmattoso84@gmail.com>
2022-08-05 15:57:40 +0100

    First version

---
+++ src/main/java/com/alura/AluraChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraChallengeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraChallengeBackendApplication.class, args);
+    }
+
+}
